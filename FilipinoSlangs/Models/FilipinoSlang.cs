ebbc72d8cc61fad25e163bf8274ff65ab74e309f
Prince Anire <prince@hiro-c.net>
2022-07-09 12:33:22 +0800

    Add `JsonPropertyName` attributes to the properties of FilipinoSlang class

--- FilipinoSlangs/Models/FilipinoSlang.cs
+++ FilipinoSlangs/Models/FilipinoSlang.cs
@@ -1,12 +1,20 @@
-﻿namespace FilipinoSlangs.Models
+﻿namespace FilipinoSlangs.Models 
 {
+    using System.Text.Json.Serialization;
     public class FilipinoSlang
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
-        public string Phrase { get; set; }
-        public string? Description { get; set; }
-        public bool HasEnglishEquivalent { get; set; }
 
+        [JsonPropertyName("phrase")]
+        public string Phrase { get; set; }
+
+        [JsonPropertyName("description")]
+        public string? Description { get; set; }
+
+
+        [JsonPropertyName("englishEquivalent")]
+        public string? EnglishEquivalent { get; set; }
 
     }
 }
