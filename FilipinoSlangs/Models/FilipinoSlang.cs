01b9d2e85d1285ba4c3a571a96eeb5cd56850413
Prince Anire <prince@hiro-c.net>
2022-07-09 11:44:40 +0800

    Add project files.

---
+++ FilipinoSlangs/Models/FilipinoSlang.cs
@@ -0,0 +1,11 @@
+﻿namespace FilipinoSlangs.Models
+{
+    public class FilipinoSlang
+    {
+        public int Id { get; set; }
+        public string Phrase { get; set; }
+        public string? Description { get; set; }
+        public
+
+    }
+}
